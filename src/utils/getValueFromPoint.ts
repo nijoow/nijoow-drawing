2a6d097e0ce296f932ecaefe5661071c06ca5d86
Woojin Lee <nijoow1127@gmail.com>
2023-06-26 00:00:17 +0900

    ✨ 드래그 BOX 추가

---
+++ src/utils/getValueFromPoint.ts
@@ -0,0 +1,18 @@
+import { Point } from "@/types/type";
+
+export const getWidthFromPoint = (point: Point) => {
+  if (point.startX === undefined || point.endX === undefined) return;
+  return Math.abs(point.endX - point.startX);
+};
+export const getHeightFromPoint = (point: Point) => {
+  if (point.startY === undefined || point.endY === undefined) return;
+  return Math.abs(point.endY - point.startY);
+};
+export const getLeftFromPoint = (point: Point) => {
+  if (point.startX === undefined || point.endX === undefined) return;
+  return Math.min(point.startX, point.endX);
+};
+export const getTopFromPoint = (point: Point) => {
+  if (point.startY === undefined || point.endY === undefined) return;
+  return Math.min(point.startY, point.endY);
+};
