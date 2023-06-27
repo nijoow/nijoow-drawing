75669dec108106d1272ef94217a7cfb45a671ba2
Woojin Lee <nijoow1127@gmail.com>
2023-06-27 22:59:56 +0900

    🎨 prettier 적용, 타입 추가

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,17 +1,28 @@
-type ModeType = "SELECT" | "PENCIL" | "VERTEX" | "SHAPE" | "TEXT" | null;
+type ModeType = 'SELECT' | 'PENCIL' | 'VERTEX' | 'SHAPE' | 'TEXT' | null
+type Option = 'FILL' | 'STROKE' | 'STORKE_WIDTH' | 'OPACITY' | null
 
 export interface Mode {
-  type: ModeType;
-  subType: string | null;
+  type: ModeType
+  subType: string | null
+}
+export interface Options {
+  fill: string
+  stroke: string
+  strokeWidth: number
+  opacity: number
 }
 
 export interface SubToolBar {
-  type: ModeType;
+  type: ModeType
+}
+
+export interface OptionsToolBar {
+  type: Option
 }
 
 export interface Point {
-  startX: number | undefined;
-  startY: number | undefined;
-  endX: number | undefined;
-  endY: number | undefined;
+  startX: number | undefined
+  startY: number | undefined
+  endX: number | undefined
+  endY: number | undefined
 }
