c3f8171cb7acf4abaf5f878fbbf86ffd0cfdad9b
Woojin Lee <nijoow1127@gmail.com>
2023-06-27 23:00:35 +0900

    🎨 prettier 적용, currentOptions atom 추가

--- src/recoil/atoms.ts
+++ src/recoil/atoms.ts
@@ -1,7 +1,12 @@
-import { Mode } from "@/types/type";
-import { atom } from "recoil";
+import { Mode, Options } from '@/types/type'
+import { atom } from 'recoil'
 
 export const modeAtom = atom<Mode>({
-  key: "Mode",
-  default: { type: "SELECT", subType: null },
-});
+  key: 'Mode',
+  default: { type: 'SELECT', subType: null },
+})
+
+export const currentOptionsAtom = atom<Options>({
+  key: 'CurrentOptions',
+  default: { fill: '#000000', stroke: '#000000', strokeWidth: 1, opacity: 1 },
+})
