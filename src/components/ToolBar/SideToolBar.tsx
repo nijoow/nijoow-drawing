597e686287fbde956eb1effd03c678ff8e2b16ba
Woojin Lee <nijoow1127@gmail.com>
2023-06-27 22:59:24 +0900

    🎨 prettier 적용

--- src/components/ToolBar/SideToolBar.tsx
+++ src/components/ToolBar/SideToolBar.tsx
@@ -1,62 +1,62 @@
-"use client";
+'use client'
 
-import { useEffect, useState } from "react";
-import { RiCursorFill, RiPencilFill, RiPenNibFill } from "react-icons/ri";
+import { useState } from 'react'
+import { RiCursorFill, RiPencilFill, RiPenNibFill } from 'react-icons/ri'
 import {
   IoShapes,
   IoText,
   IoSquare,
   IoTriangle,
   IoEllipse,
-} from "react-icons/io5";
-import { useRecoilState } from "recoil";
-import { modeAtom } from "@/recoil/atoms";
-import { SubToolBar } from "@/types/type";
+} from 'react-icons/io5'
+import { useRecoilState } from 'recoil'
+import { modeAtom } from '@/recoil/atoms'
+import { SubToolBar } from '@/types/type'
 
 export default function SideToolBar() {
-  const [mode, setMode] = useRecoilState(modeAtom);
+  const [mode, setMode] = useRecoilState(modeAtom)
   const [openSubToolBar, setOpenSubToolBar] = useState<SubToolBar>({
     type: null,
-  });
+  })
 
   return (
-    <div className="flex flex-col justify-center items-center left-4 fixed gap-2 bg-gray-600 text-white min-h-fit w-16 py-2 z-10 rounded-lg overflow-visible">
+    <div className="fixed z-10 flex flex-col items-center justify-center w-16 gap-2 py-2 overflow-visible text-white bg-gray-600 rounded-lg left-4 min-h-fit">
       <button
         type="button"
         className="p-2"
-        onClick={() => setMode({ type: "SELECT", subType: "" })}
+        onClick={() => setMode({ type: 'SELECT', subType: '' })}
       >
         <RiCursorFill size={20} />
       </button>
       <button
         type="button"
         className="p-2"
-        onClick={() => setMode({ type: "PENCIL", subType: "" })}
+        onClick={() => setMode({ type: 'PENCIL', subType: '' })}
       >
         <RiPencilFill size={20} />
       </button>
       <button
         type="button"
         className="p-2"
-        onClick={() => setMode({ type: "VERTEX", subType: "" })}
+        onClick={() => setMode({ type: 'VERTEX', subType: '' })}
       >
         <RiPenNibFill size={20} />
       </button>
-      <div className="flex items-center justify-center relative w-full py-2">
+      <div className="relative flex items-center justify-center w-full py-2">
         <button
           type="button"
-          onClick={() => setOpenSubToolBar({ type: "SHAPE" })}
+          onClick={() => setOpenSubToolBar({ type: 'SHAPE' })}
         >
           <IoShapes size={20} />
         </button>
-        {openSubToolBar.type === "SHAPE" && (
-          <div className="flex absolute left-full top-0 bg-red-500 ">
+        {openSubToolBar.type === 'SHAPE' && (
+          <div className="absolute top-0 flex bg-red-500 left-full ">
             <button
               type="button"
               className="p-2"
               onClick={() => {
-                setMode({ type: "SHAPE", subType: "RECTANGLE" });
-                setOpenSubToolBar({ type: null });
+                setMode({ type: 'SHAPE', subType: 'RECTANGLE' })
+                setOpenSubToolBar({ type: null })
               }}
             >
               <IoSquare size={20} />
@@ -65,8 +65,8 @@ export default function SideToolBar() {
               type="button"
               className="p-2"
               onClick={() => {
-                setMode({ type: "SHAPE", subType: "ELLIPSE" });
-                setOpenSubToolBar({ type: null });
+                setMode({ type: 'SHAPE', subType: 'ELLIPSE' })
+                setOpenSubToolBar({ type: null })
               }}
             >
               <IoEllipse size={20} />
@@ -75,8 +75,8 @@ export default function SideToolBar() {
               type="button"
               className="p-2"
               onClick={() => {
-                setMode({ type: "SHAPE", subType: "TRIANGLE" });
-                setOpenSubToolBar({ type: null });
+                setMode({ type: 'SHAPE', subType: 'TRIANGLE' })
+                setOpenSubToolBar({ type: null })
               }}
             >
               <IoTriangle size={20} />
@@ -87,10 +87,10 @@ export default function SideToolBar() {
       <button
         type="button"
         className="p-2"
-        onClick={() => setMode({ type: "TEXT", subType: "" })}
+        onClick={() => setMode({ type: 'TEXT', subType: '' })}
       >
         <IoText size={20} />
       </button>
     </div>
-  );
+  )
 }
