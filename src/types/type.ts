002e7e0843a2187ba38f5857c4258499007f79df
Woojin Lee <nijoow1127@gmail.com>
2023-07-11 23:28:05 +0900

    🎨 type, interface 추가

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,5 +1,6 @@
 type ModeType = 'SELECT' | 'PENCIL' | 'VERTEX' | 'SHAPE' | 'TEXT' | null
 type Option = 'FILL' | 'STROKE' | 'STORKE_WIDTH' | 'OPACITY' | null
+export type Direction = 'TL' | 'T' | 'TR' | 'L' | 'R' | 'BL' | 'B' | 'BR' | null
 
 export interface Mode {
   type: ModeType
@@ -25,4 +26,11 @@ export interface Point {
   startY: number | undefined
   endX: number | undefined
   endY: number | undefined
+}
+
+export interface ShapeData {
+  width: number | null
+  height: number | null
+  center: { x: number | null; y: number | null }
+  rotate: number | null
 }
