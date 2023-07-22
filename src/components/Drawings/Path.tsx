20c8ef8692c8a53d5a261379d1f1cde3d12fac12
Woojin Lee <nijoow1127@gmail.com>
2023-07-22 23:56:38 +0900

    ✨ 선 모양 추가

---
+++ src/components/Drawings/Path.tsx
@@ -0,0 +1,42 @@
+import { Drawing } from '@/types/type'
+import React from 'react'
+
+const Path = ({ drawing }: { drawing: Drawing }) => {
+  const left = drawing.center.x - drawing.width / 2
+  const top = drawing.center.y - drawing.height / 2
+  return (
+    <svg
+      id={drawing.id}
+      viewBox={`${left} ${top} ${drawing.width} ${drawing.height}`}
+      width={drawing.width}
+      height={drawing.height}
+      overflow={'visible'}
+      className={`absolute `}
+      style={{
+        left,
+        top,
+        rotate: `${drawing.rotate}deg`,
+      }}
+    >
+      <polygon
+        id={drawing.id}
+        d={drawing.vertexs
+          .map((vertex, index) => {
+            if (index === 0) {
+              return `M${vertex.x} ${vertex.y}`
+            } else {
+              return `L${vertex.x} ${vertex.y}`
+            }
+          })
+          .join(' ')}
+        fill={drawing.fill}
+        stroke={drawing.stroke}
+        strokeWidth={drawing.strokeWidth}
+        opacity={drawing.opacity}
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export default Path
