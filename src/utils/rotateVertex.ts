660891801c181f3515eae3c7c3a4d30b16dadf53
Woojin Lee <nijoow1127@gmail.com>
2023-07-30 22:04:38 +0900

    🎨 rotateVertex 함수 분리

---
+++ src/utils/rotateVertex.ts
@@ -0,0 +1,17 @@
+export const rotateVertex = (
+  vertexX: number,
+  vertexY: number,
+  centerX: number,
+  centerY: number,
+  rotate: number,
+) => {
+  const r = rotate * (Math.PI / 180)
+
+  const x =
+    (vertexX - centerX) * Math.cos(r) - (vertexY - centerY) * Math.sin(r)
+
+  const y =
+    (vertexX - centerX) * Math.sin(r) + (vertexY - centerY) * Math.cos(r)
+
+  return { x, y }
+}
